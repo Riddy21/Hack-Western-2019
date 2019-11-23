952bff258180e74e01c5e9dabe4c5e8fc8144cba
Ridvan Song <ridvansong@hotmail.com>
2019-11-23 12:59:18 -0500

    Edit window with all pages added as "populate" functions

--- Window.py
+++ Window.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 
+
 class Window():
     def __init__(self):
         # Setup window
@@ -7,6 +8,9 @@ class Window():
         self.username = tk.StringVar()
         self.password = tk.StringVar()
         self.mainFrame = tk.Frame(self.window)
+        self.transactionFrame = tk.Frame(self.window)
+        self.groupsFrame = tk.Frame(self.window)
+        self.membersFrame = tk.Frame(self.window)
 
         self.window.title("Test")
         self.window.geometry("500x800")
@@ -14,6 +18,14 @@ class Window():
 
         self.populateMain()
         (self.window).mainloop()
+
+    def destroyFrame(self, frame):
+        frame.destroy()
+
     def populateMain(self):
         self.mainFrame.pack()
-        print(1)
+        tk.Label(self.mainFrame, text = "1").pack()
+
+    def populateTransaction(self):
+        self.destroyFrame(self.mainFrame)
+        self.transactionFrame.pack()
