6049b9ebaaa90ac229dd7800a0807234e67619b6
Ridvan Song <ridvansong@hotmail.com>
2019-11-23 13:39:06 -0500

    Created all frames and currently populating main

--- Window.py
+++ Window.py
@@ -5,27 +5,55 @@ class Window():
     def __init__(self):
         # Setup window
         self.window = tk.Tk()
-        self.username = tk.StringVar()
-        self.password = tk.StringVar()
+
+        self.monthlyBudget = tk.StringVar()
+
+        #Groups
+
+
+        #Frames
         self.mainFrame = tk.Frame(self.window)
         self.transactionFrame = tk.Frame(self.window)
         self.groupsFrame = tk.Frame(self.window)
         self.membersFrame = tk.Frame(self.window)
+        self.profileFrame = tk.Frame(self.window)
+        self.holdFrame = tk.Frame(self.window) # holder frame
 
         self.window.title("Test")
         self.window.geometry("500x800")
         self.window.resizable(1, 1)
 
         self.populateMain()
+        self.populateTransaction()
+        self.populateGroups()
+        self.populateMembers()
+        self.populateProfile()
+
+        self.switchFrame(self.mainFrame,self.holdFrame)
         (self.window).mainloop()
 
-    def destroyFrame(self, frame):
-        frame.destroy()
+    def switchFrame(self, frame, prevFrame):
+        prevFrame.pack_forget()
+        frame.pack()
 
     def populateMain(self):
-        self.mainFrame.pack()
-        tk.Label(self.mainFrame, text = "1").pack()
+        self.monthlyBudget.set("500")
+        tk.Label(self.mainFrame, text = "Your Monthly Budget is:").pack()
+        tk.Label(self.mainFrame, text = self.monthlyBudget.get()).pack()
+        tk.Button(self.mainFrame,text = "Add a Transaction",command = lambda: self.switchFrame(self.transactionFrame,self.mainFrame)).pack()
+        profiles = tk.Frame(self.mainFrame).pack()
+
+        tk.Button(profiles, text = "Profile")
 
     def populateTransaction(self):
-        self.destroyFrame(self.mainFrame)
-        self.transactionFrame.pack()
+        tk.Label(self.transactionFrame,text = "2").pack()
+        tk.Button(self.transactionFrame,text = "bop",command = lambda: self.switchFrame(self.mainFrame,self.transactionFrame)).pack()
+
+    def populateGroups(self):
+        tk.Label(self.groupsFrame, text="empty").pack()
+
+    def populateMembers(self):
+        tk.Label(self.membersFrame, text="empty").pack()
+
+    def populateProfile(self):
+        tk.Label(self.profileFrame, text="empty").pack()
